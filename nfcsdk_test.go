eee4f8a964ac3a9a39d9975ae9fe42527dbfaabd
Marko Kungla <marko@mkungla.dev>
2023-11-21 05:32:37 +0200

    devops: add FormatByteSlice tests
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

---
+++ nfcsdk_test.go
@@ -0,0 +1,29 @@
+// Copyright 2023 The Happy Authors
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file.
+
+package nfcsdk_test
+
+import (
+	"testing"
+
+	"github.com/happy-sdk/nfcsdk/internal/helpers"
+)
+
+func TestFormatByteSlice(t *testing.T) {
+	// Test case 1: An empty byte slice should return an empty string.
+	emptySlice := []byte{}
+	result := helpers.FormatByteSlice(emptySlice)
+	expected := ""
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+
+	// Test case 2: A byte slice with some data should be formatted as expected.
+	dataSlice := []byte{72, 101, 108, 108, 111} // ASCII values for "Hello"
+	result = helpers.FormatByteSlice(dataSlice)
+	expected = "48:65:6C:6C:6F" // Hexadecimal representation of ASCII values
+	if result != expected {
+		t.Errorf("Expected '%s', but got '%s'", expected, result)
+	}
+}
