27bf353f016ffd1367516fcddbaa3ff8e7d5c39f
Marko Kungla <marko@mkungla.dev>
2023-11-21 20:05:50 +0200

    feat: init apdu package
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

---
+++ apdu/apdu.go
@@ -0,0 +1,3 @@
+// Package apdu implements parsing and conversion of Application Protocol Data Units (APDU) which is the communication format between a card and off-card applications.
+
+package apdu
