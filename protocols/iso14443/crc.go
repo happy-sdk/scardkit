35b00b037997d34a10e6205e33f8f3d73c8e61a5
Marko Kungla <marko@mkungla.dev>
2023-11-21 23:14:39 +0200

    devops: move ISO 14443 CRC to right import path
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

---
+++ protocols/iso14443/crc.go
@@ -0,0 +1,67 @@
+// Copyright 2023 The Happy Authors
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file.
+
+package iso14443
+
+import "bytes"
+
+// CalculateCRCA calculates the CRC-A checksum for a given slice of data.
+// This function implements the CRC-A algorithm used in ISO/IEC 14443 Type A
+// standard, suitable for NFC communication. The function returns a 2-byte slice
+// containing the CRC in little-endian format.
+func CalculateCRCA(data []byte) []byte {
+	poly := uint16(0x1021)
+	crc := uint16(0xC6C6)
+
+	for _, b := range data {
+		curByte := reflectByte(uint16(b))
+		crc ^= curByte << 8
+		for i := 0; i < 8; i++ {
+			if crc&0x8000 != 0 {
+				crc = (crc << 1) ^ poly
+			} else {
+				crc = crc << 1
+			}
+		}
+	}
+
+	crc = reflectUint16(crc)
+	return []byte{byte(crc & 0xFF), byte(crc >> 8)}
+}
+
+// VerifyCRCA compares the calculated CRC-A checksum of the provided data
+// against a received CRC checksum. This is typically used to validate
+// the integrity of data received from NFC tags. The function returns true
+// if the calculated CRC matches the received CRC, indicating that the data
+// is likely intact and uncorrupted.
+func VerifyCRCA(data []byte, crc []byte) bool {
+	calculatedCRC := CalculateCRCA(data)
+	return bytes.Equal(calculatedCRC, crc)
+}
+
+// reflectByte reverses the order of the last 8 bits in x.
+func reflectByte(x uint16) uint16 {
+	reflection := uint16(0)
+	for i := uint16(0); i < 8; i++ {
+		if x&(1<<i) != 0 {
+			reflection |= 1 << (7 - i)
+		}
+	}
+	return reflection
+}
+
+// reflectUint16 reverses the order of the last 16 bits in x.
+func reflectUint16(x uint16) uint16 {
+	reflection := uint16(0)
+	for i := uint16(0); i < 16; i++ {
+		if x&(1<<i) != 0 {
+			reflection |= 1 << (15 - i)
+		}
+	}
+	return reflection
+}
+
+// ref: found ideas for implementation from following links
+// http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
+// https://hub.zhovner.com/tools/nfc/
---
+++ protocols/iso14443/crc_test.go
@@ -0,0 +1,64 @@
+// Copyright 2023 The Happy Authors
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file.
+
+package iso14443
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCalculateCRCA(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []byte
+		want  []byte
+	}{
+		{
+			name:  "Test 1",
+			input: []byte{0x00, 0x04, 0x04, 0x02, 0x01, 0x00, 0x0F, 0x03},
+			want:  []byte{0x80, 0x91},
+		},
+		// Add more test cases here
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CalculateCRCA(tt.input); !bytes.Equal(got, tt.want) {
+				t.Errorf("CalculateCRCA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVerifyCRCA(t *testing.T) {
+	tests := []struct {
+		name        string
+		data        []byte
+		receivedCRC []byte
+		want        bool
+	}{
+		{
+			name:        "Valid CRC",
+			data:        []byte{0x00, 0x04, 0x04, 0x02, 0x01, 0x00, 0x0F, 0x03},
+			receivedCRC: []byte{0x80, 0x91},
+			want:        true,
+		},
+		{
+			name:        "Invalid CRC",
+			data:        []byte{0x00, 0x04, 0x04, 0x02, 0x01, 0x00, 0x0F, 0x03},
+			receivedCRC: []byte{0x00, 0x00},
+			want:        false,
+		},
+		// Additional test cases can be added here
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := VerifyCRCA(tt.data, tt.receivedCRC); got != tt.want {
+				t.Errorf("VerifyCRCA() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
